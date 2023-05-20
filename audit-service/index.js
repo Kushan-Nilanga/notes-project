f1be5a547691134d2265d989bc97104fa7cf0ca0
Don Athalage <dknathalage@gmail.com>
2023-05-20 20:23:28 +1000

    add mongo connection

--- audit-service/index.js
+++ audit-service/index.js
@@ -1,8 +1,27 @@
 // simple express server
 const express = require("express");
+const mongoose = require("mongoose");
+
+const service_name = "audit-service";
+const mongo_host = process.env.MONGO_SERVICE_HOST;
+const mongo_username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const mongo_password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const mongo_uri = `mongodb://${mongo_username}:${mongo_password}@${mongo_host}:27017/notes?authSource=admin`;
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello from audit-service!"));
+mongoose
+  .connect(mongo_uri, { useNewUrlParser: true })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log(`${service_name} connected to MongoDB`);
+  });
 
-app.listen(port, () => console.log(`audit-service listening on port ${port}!`));
+app.get("/", (req, res) => res.send(`Hello from ${service_name}!`));
+
+app.listen(port, () =>
+  console.log(`${service_name} listening on port ${port}!`)
+);
--- auth-service/index.js
+++ auth-service/index.js
@@ -1,8 +1,27 @@
 // simple express server
 const express = require("express");
+const mongoose = require("mongoose");
+
+const service_name = "auth-service";
+const mongo_host = process.env.MONGO_SERVICE_HOST;
+const mongo_username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const mongo_password = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const mongo_uri = `mongodb://${mongo_username}:${mongo_password}@${mongo_host}:27017/notes?authSource=admin`;
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello from auth-service!"));
+mongoose
+  .connect(mongo_uri, { useNewUrlParser: true })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log(`${service_name} connected to MongoDB`);
+  });
 
-app.listen(port, () => console.log(`auth-service listening on port ${port}!`));
+app.get("/", (req, res) => res.send(`Hello from ${service_name}!`));
+
+app.listen(port, () =>
+  console.log(`${service_name} listening on port ${port}!`)
+);
