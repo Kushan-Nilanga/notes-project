0a13d37cc7d589390abc733e183e060642ee61d2
Don Athalage <dknathalage@gmail.com>
2023-05-20 15:00:04 +1000

    remote extra repos

--- image-repos.tf
+++ image-repos.tf
@@ -6,42 +6,6 @@ resource "google_artifact_registry_repository" "notes" {
   format        = "DOCKER"
 }
 
-# auth
-resource "google_artifact_registry_repository" "auth" {
-  location      = local.google.region
-  repository_id = "auth"
-  description   = "auth docker image"
-  format        = "DOCKER"
-}
-
-# audit
-resource "google_artifact_registry_repository" "audit" {
-  location      = local.google.region
-  repository_id = "audit"
-  description   = "audit docker image"
-  format        = "DOCKER"
-}
-
-# bff
-resource "google_artifact_registry_repository" "bff" {
-  location      = local.google.region
-  repository_id = "bff"
-  description   = "bff docker image"
-  format        = "DOCKER"
-}
-
 output "gcr_notes_repository" {
   value = "https://console.cloud.google.com/artifacts/docker/${local.google.project_id}/${local.google.region}/${google_artifact_registry_repository.notes.repository_id}?project=${local.google.project_id}"
-}
-
-output "gcr_auth_repository" {
-  value = "https://console.cloud.google.com/artifacts/docker/${local.google.project_id}/${local.google.region}/${google_artifact_registry_repository.auth.repository_id}?project=${local.google.project_id}"
-}
-
-output "gcr_audit_repository" {
-  value = "https://console.cloud.google.com/artifacts/docker/${local.google.project_id}/${local.google.region}/${google_artifact_registry_repository.audit.repository_id}?project=${local.google.project_id}"
-}
-
-output "gcr_bff_repository" {
-  value = "https://console.cloud.google.com/artifacts/docker/${local.google.project_id}/${local.google.region}/${google_artifact_registry_repository.bff.repository_id}?project=${local.google.project_id}"
 }
