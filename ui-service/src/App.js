1a047790a189a2315b61e022926223d925ae20c4
Don Athalage <dknathalage@gmail.com>
2023-05-27 18:39:40 +1000

    ui update and env var setup

--- ui-service/src/App.js
+++ ui-service/src/App.js
@@ -1,5 +1,5 @@
 /* IMPORTS */
-import { Col, Container, Nav, Row, Button, Form } from "react-bootstrap";
+import { Col, Container, Nav, Row, Button, Form, Badge } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -71,37 +71,34 @@ const SideBar = ({ notes_props, onActive, onDelete }) => {
 
   return (
     <Nav className="w-auto">
-      {notes && (
-        <Nav.Item className="w-100 border-bottom">
-          {notes.map((note) => {
-            return (
-              <div className="d-flex justify-content-between">
-                <Nav.Link
-                  key={note._id}
-                  className="w-100"
-                  onClick={() => onActive(note._id)}
-                >
-                  {note.title}
-                </Nav.Link>
-                <Button
-                  className="btn-sm m-1 p-1"
-                  variant="danger"
-                  onClick={() => {
-                    try {
-                      delete_note(note._id);
-                      onDelete();
-                    } catch (err) {
-                      console.log(err);
-                    }
-                  }}
-                >
-                  X
-                </Button>
-              </div>
-            );
-          })}
-        </Nav.Item>
-      )}
+      {notes &&
+        notes.map((note) => (
+          <Nav.Item className="w-100 border-bottom" key={note._id}>
+            <div className="d-flex justify-content-between">
+              <Nav.Link
+                key={note._id}
+                className="w-100"
+                onClick={async () => await onActive(note._id)}
+              >
+                {note.title}
+              </Nav.Link>
+              <Button
+                className="btn-sm m-1 p-1"
+                variant="danger"
+                onClick={async () => {
+                  try {
+                    await delete_note(note._id);
+                    await onDelete();
+                  } catch (err) {
+                    console.log(err);
+                  }
+                }}
+              >
+                X
+              </Button>
+            </div>
+          </Nav.Item>
+        ))}
     </Nav>
   );
 };
@@ -117,24 +114,24 @@ const Login = (props) => {
     <Form className="m-3">
       <Form.Group className="mb-1" controlId="formBasicEmail">
         <Form.Control
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={async (e) => await setEmail(e.target.value)}
           type="email"
           className="mb-1"
           placeholder="Enter email"
           value={email}
         />
         <Form.Control
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={async (e) => await setPassword(e.target.value)}
           type="password"
           placeholder="Password"
           value={password}
         />
       </Form.Group>
       <Button
-        onClick={() => {
+        onClick={async () => {
           try {
-            login(email, password);
-            onLogin();
+            await login(email, password);
+            await onLogin();
           } catch (err) {
             console.log(err);
           }
@@ -162,10 +159,74 @@ const Login = (props) => {
   );
 };
 
+const Note = ({ note_prop, onUpdate }) => {
+  const [titleInFocus, setTitleFocus] = useState(false);
+  const [contentInFocus, setContentFocus] = useState(true);
+  const [note, setNote] = useState(note_prop);
+
+  useEffect(() => {
+    try {
+      if (note_prop) {
+        setNote({
+          title: note_prop.title,
+          content: note_prop.content,
+          id: note_prop.id,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [note_prop]);
+
+  useEffect(() => {
+    onUpdate(note);
+  }, [note]);
+
+  return (
+    note && (
+      <div className="h-100">
+        <h1 className="m-0 py-2">
+          {!titleInFocus && (
+            <span onClick={async () => await setTitleFocus(true)}>
+              {note.title}
+            </span>
+          )}
+          {titleInFocus && (
+            <input
+              className="w-100"
+              onChange={async (e) =>
+                await setNote({ ...note, title: e.target.value })
+              }
+              onBlur={async () => await setTitleFocus(false)}
+              value={note.title}
+            />
+          )}
+        </h1>
+        {!contentInFocus && (
+          <p onClick={async () => await setContentFocus(true)}>
+            {note.content}
+          </p>
+        )}
+        {contentInFocus && (
+          <textarea
+            className="w-100 vh-100"
+            onChange={async (e) =>
+              await setNote({ ...note, content: e.target.value })
+            }
+            onBlur={async () => await setContentFocus(false)}
+            value={note.content}
+          />
+        )}
+      </div>
+    )
+  );
+};
+
 function App() {
   const [notes, setNotes] = useState([]);
   const [active, setActive] = useState(null);
   const [token, setToken] = useState(null);
+  const [isUpdating, setUpdating] = useState(false);
 
   useEffect(() => {
     get_notes()
@@ -183,57 +244,95 @@ function App() {
       <Row className="vh-100">
         <Col xs={3} className="bg-light w-10 p-0">
           <Login
-            onLogin={() => {
-              setToken(axios.defaults.headers.common["Authorization"]);
-              get_notes()
-                .then((res) => {
-                  console.log(res.data);
-                  setNotes(res.data);
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
+            onLogin={async () => {
+              await setToken(axios.defaults.headers.common["Authorization"]);
+              const res = await get_notes();
+              await setNotes(res.data);
             }}
           />
+
           {!token && <span className="text-danger p-3">Not logged in</span>}
-          {token && <span className="text-success p-3">Logged in</span>}
+          {token && (
+            <div>
+              <span className="text-success p-3">Logged in</span>
+              <Button
+                className="btn btn-danger btn-sm"
+                onClick={async () => {
+                  axios.defaults.headers.common["Authorization"] = "";
+                  await setToken(null);
+                }}
+              >
+                Logout
+              </Button>
+            </div>
+          )}
           <hr />
-          <SideBar
-            notes_props={notes}
-            onActive={(id) => {
-              const atv = notes.find((note) => note._id === id);
-              console.log(atv);
-              setActive(atv);
-            }}
-            onDelete={async () => {
-              try {
-                const res = await get_notes();
-                setNotes(res.data);
-                setActive({ title: "", content: "" });
-              } catch (err) {
-                console.log(err);
-              }
-            }}
-          />
+          {token && (
+            <SideBar
+              notes_props={notes}
+              onActive={async (id) => {
+                const atv = notes.find((note) => note._id === id);
+                atv["id"] = id;
+                await setActive(atv);
+              }}
+              onDelete={async () => {
+                try {
+                  const res = await get_notes();
+                  await setNotes(res.data);
+                  await setActive(null);
+                } catch (err) {
+                  console.log(err);
+                }
+              }}
+            />
+          )}
           <hr />
-          <Button
-            className="btn-sm m-3 btn-secondary"
-            onClick={async () => {
-              setActive(null);
-              try {
-                await create_note("New Note", "Click here to edit the note");
-                const res = await get_notes();
-                setNotes(res.data);
-              } catch (err) {
-                console.log(err);
-              }
-            }}
-          >
-            New Note
-          </Button>
+          {token && (
+            <Button
+              className="btn-sm m-3 btn-secondary"
+              onClick={async () => {
+                await setActive(null);
+                try {
+                  await create_note("New Note", "Click here to edit the note");
+                  const res = await get_notes();
+                  setNotes(res.data);
+                } catch (err) {
+                  console.log(err);
+                }
+              }}
+            >
+              New Note
+            </Button>
+          )}
+          {isUpdating && (
+            <Badge pill bg="warning" text="dark">
+              Updating
+            </Badge>
+          )}
         </Col>
         <Col className="pt-5 ps-5">
-          <Note note_prop={active} />
+          {active && token && (
+            <Note
+              isUpdating={isUpdating}
+              note_prop={active}
+              onUpdate={async (note) => {
+                await setUpdating(true);
+                const updated = await update_note(
+                  note.id,
+                  note.title,
+                  note.content
+                );
+                get_notes()
+                  .then((res) => {
+                    setNotes(res.data);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+                await setUpdating(false);
+              }}
+            />
+          )}
         </Col>
       </Row>
     </Container>
@@ -241,59 +340,3 @@ function App() {
 }
 
 export default App;
-
-const Note = ({ note_prop }) => {
-  const [titleInFocus, setTitleFocus] = useState(false);
-  const [contentInFocus, setContentFocus] = useState(true);
-  const [note, setNote] = useState(note_prop);
-
-  useEffect(() => {
-    try {
-      if (note_prop) {
-        setNote({
-          title: note_prop.title,
-          content: note_prop.content,
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }, [note_prop]);
-
-  return (
-    note && (
-      <div className="h-100">
-        <h1 className="m-0 py-2">
-          {!titleInFocus && (
-            <span onClick={() => setTitleFocus(true)}>
-              {note.title || "Edit Title"}
-            </span>
-          )}
-          {titleInFocus && (
-            <input
-              className="w-100"
-              onChange={(e) =>
-                setNote({ title: e.target.value, content: note.content })
-              }
-              onBlur={() => setTitleFocus(false)}
-              value={note.title || "Edit Title"}
-            />
-          )}
-        </h1>
-        {!contentInFocus && (
-          <p onClick={() => setContentFocus(true)}>
-            {note.content || "Edit Content"}
-          </p>
-        )}
-        {contentInFocus && (
-          <textarea
-            className="w-100 vh-100"
-            onChange={(e) => setNote({ ...note, content: e.target.value })}
-            onBlur={() => setContentFocus(false)}
-            value={note.content || "Edit Content"}
-          />
-        )}
-      </div>
-    )
-  );
-};
